8d9b5a6a760e00b0882a0f11612e14b92a02916e
999MeshDi666 <havkpzz@gmail.com>
2022-04-08 01:22:01 +0600

    google forms and success page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,6 +5,8 @@ showOverlay.forEach((showOverlay) => {
         overlay.classList.toggle('overlay_show')});
 });
 
+
+
 // let showOverlay = document.getElementById('show_overlay');
 // let hideOverlay = document.getElementById('hide_overlay');
 // function Overlay(show_hide, overlay){
@@ -42,71 +44,72 @@ showOverlay.forEach((showOverlay) => {
 //     let formData = new FormData(form);
 //     $.ajax({
 //         type: "POST",
-//         url: '',
+//         url: 'https://docs.google.com/forms/d/e/1FAIpQLScQvxtYBZYX-z_sPh_BICnoZJhgb_VZim6gDXg9gNTorc6_tQ/formResponse',
 //         data: formData,
 //         dataType: 'html',
 //         processData: false,
 //         contentType: false,
 //         success: function(){
 //             alert('POST success');
-            
 //         },
 //         erorr: function(){
 //             alert('POST error');
 //         }
 //     })    
-}
+// }
 
-
+// let submit = document.querySelector('.form__submit');
+// submit.addEventListener('click', sendRequest);
 
 
 
 // Process contact form
-$('#form').submit(function(event) {
-    event.preventDefault();
+// $('#form').submit(function(event) {
+//     event.preventDefault();
    
-    setTimeout(function() {
-        // Get data
-      var data = {
-        'entry.1381807465': $('#team').val(),
-        'entry.689161790': $('#study_place').val(),
-        'entry.277358756': $('#sections').val(),
-        'entry.964255080': $('#participant1').val(),
-        'entry.702140556': $('#email').val(),
-        'entry.160789097': $('#tel').val(),
-        'entry.1752364032': $('#participant2').val(),
-        'entry.1721330725': $('#participant3').val(),
-        'entry.1823584633': $('#participant4').val(),
-        'entry.1726591051': $('#participant5').val(),
-      };
+//     setTimeout(function() {
+//         // Get data
+//       var data = {
+//         'entry.1381807465': $('#team').val(),
+//         'entry.689161790': $('#study_place').val(),
+//         'entry.277358756': $('#sections').val(),
+//         'entry.964255080': $('#participant1').val(),
+//         'entry.702140556': $('#email').val(),
+//         'entry.160789097': $('#tel').val(),
+//         'entry.1752364032': $('#participant2').val(),
+//         'entry.1721330725': $('#participant3').val(),
+//         'entry.1823584633': $('#participant4').val(),
+//         'entry.1726591051': $('#participant5').val(),
+//       };
   
-      // Validate form
-      var formSuccess = true;
-      Object.keys(data).forEach(function(key, index) {
-        if (!data[key]) {
-          formSuccess = false;
-          alert('Please complete all fields');
-        }
-      });
+//       // Validate form
+//       var formSuccess = true;
+//       Object.keys(data).forEach(function(key, index) {
+//         if (!data[key]) {
+//           formSuccess = false;
+//           alert('Please complete all fields');
+//         }
+//       });
   
-      if (formSuccess) {
-        // Send request
-        $.ajax({
-          url: 'https://docs.google.com/forms/d/e/1FAIpQLScQvxtYBZYX-z_sPh_BICnoZJhgb_VZim6gDXg9gNTorc6_tQ/formResponse',
-          type: 'POST',
-          crossDomain: true,
-          dataType: "xml",
-          data: data,
-          success: function(jqXHR, textStatus, errorThrown) {
-            console.log('Enter on success');
-            alert('Message sent!');
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-            console.log('Enter on error');
-            alert('Error');
-          }
-        });
-      }
-    }, 300);
-  });
+//       if (formSuccess) {
+//         // Send request
+//         $.ajax({
+//           url: 'https://docs.google.com/forms/d/e/1FAIpQLScQvxtYBZYX-z_sPh_BICnoZJhgb_VZim6gDXg9gNTorc6_tQ/formResponse',
+//           type: 'POST',
+//           crossDomain: true,
+//           dataType: "html",
+//           data: data,
+//           success: function(jqXHR, textStatus, errorThrown) {
+//             console.log('Enter on success');
+//             alert('Message sent!');
+//           },
+//           error: function(jqXHR, textStatus, errorThrown) {
+//             console.log('Enter on error');
+//             alert('Error');
+//           }
+//         });
+//       }
+//     }, 300);
+//   });
   
+
